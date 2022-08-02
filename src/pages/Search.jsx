a41b9b6c971b74fddfd6a273fc61664aae082355
Lucas <lucas_emanuel_sc@hotmail.com>
2022-08-02 11:19:32 -0300

    terminei a 5 NO PIQUE!

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,10 +2,37 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 export default class Search extends Component {
+  state = {
+    searchValue: '',
+  }
+
+  inputChange = ({ target }) => {
+    this.setState({
+      searchValue: target.value,
+    });
+  }
+
   render() {
+    const minimumInputLength = 2;
+    const { searchValue } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+          <input
+            data-testid="search-artist-input"
+            type="text"
+            value={ searchValue }
+            onChange={ this.inputChange }
+          />
+          <button
+            type="submit"
+            data-testid="search-artist-button"
+            disabled={ searchValue.length < minimumInputLength }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
